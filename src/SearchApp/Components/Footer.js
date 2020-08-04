cdd1c551ae99e84a56521150eb5b925775d92447
Harekrishna Jena <hkjena96@gmail.com>
2020-08-04 23:50:26 +0530

    added auto generated copyright

--- src/SearchApp/Components/Footer.js
+++ src/SearchApp/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className='mt-10 flex flex-wrap justify-center bg-purple-200 items-center py-1 fixed bottom-0 w-full'>
-      &copy; <p>Copyright 2020.</p>
+      &copy; <p>Copyright {new Date().getFullYear()}.</p>
       <p>Made With </p>
       <i className='far fa-heart m-2 font-bold text-red-600'></i>
       <span>
