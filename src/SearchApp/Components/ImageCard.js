4761dd32118d06d26810c1d28337f9214a8246ab
Harekrishna Jena <hkjena96@gmail.com>
2020-08-04 23:58:42 +0530

    small issue fixed

--- src/SearchApp/Components/ImageCard.js
+++ src/SearchApp/Components/ImageCard.js
@@ -2,11 +2,6 @@ import React, { useEffect, useState } from 'react';
 
 const ImageCard = ({ image }) => {
   const { urls, description, likes } = image;
-  const ImageRef = React.createRef();
-  const [visivility, Setvisivility] = useState(0);
-  useEffect(() => {
-    ImageRef.current.addEventListener('load', () => Setvisivility(1));
-  }, [ImageRef]);
   return (
     <>
       <div class='hover column'>
@@ -19,11 +14,9 @@ const ImageCard = ({ image }) => {
               height: 'auto',
               marginBottom: '0.5vw',
             }}
-            ref={ImageRef}
           />
           <div
             className='likes text-white bg-black bg-opacity-50 rounded px-2 py-2'
-            // style={{ opacity: visivility }}
           >
             <i className='far fa-heart mr-1 font-bold text-xl'></i>
             {<span className='text-xl'>{likes}</span> || (
