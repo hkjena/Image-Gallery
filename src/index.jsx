bc11e7e394e79799284484e902992152f7e30b60
Harekrishna Jena <hkjena96@gmail.com>
2023-12-23 02:51:25 +0900

    removed unnecessary css

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./SearchApp/app";
-import "./assets/main.css";
+import "./assets/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
