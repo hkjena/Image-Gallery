2e8157c1777cc3d7bfb30597d364589a21e5dee4
Harekrishna Jena <hkjena96@gmail.com>
2023-12-23 02:05:12 +0900

    migrated to vite and latest react

--- src/APIs/unsplash.js
+++ src/APIs/unsplash.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
-const KEY = 'ePCbcySRDcFwVECg8VuSzCXm1Aq7jwmrBMn-2ob4tZE';
+import axios from "axios";
+const KEY = "ePCbcySRDcFwVECg8VuSzCXm1Aq7jwmrBMn-2ob4tZE";
 
 export default axios.create({
-  baseURL: 'https://api.unsplash.com',
+  baseURL: "https://api.unsplash.com",
   headers: {
     Authorization: `Client-ID ${KEY}`,
   },
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './SearchApp/app';
-import './assets/main.css';
-
-ReactDOM.render(<App />, document.querySelector('#root'));
