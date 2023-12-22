85170905a9a4e29841780465bd908556aec6cb48
Harekrishna Jena <hkjena96@gmail.com>
2023-12-23 02:40:52 +0900

    --amend
    migrated to vite and latest react

---
+++ src/SearchApp/components/ImageList.jsx
@@ -0,0 +1,15 @@
+import ImageCard from "./ImageCard";
+
+const ImageList = ({ photos }) => {
+  return (
+    <>
+      <div id='photos' className='photos'>
+        {photos.map((image) => {
+          return <ImageCard key={image.id} image={image} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default ImageList;
