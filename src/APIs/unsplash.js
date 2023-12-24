a18e4714e381f21160df2904017b0e43c5f1e241
Harekrishna Jena <hkjena96@gmail.com>
2023-12-24 14:41:50 +0900

    added environment variables

--- src/APIs/unsplash.js
+++ src/APIs/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: `Client-ID ${process.env.KEY}`,
+    Authorization: `Client-ID ${import.meta.env.VITE_UNSPLASH_API_KEY}`,
   },
 });
